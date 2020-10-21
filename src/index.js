adc736ce2e8c89461c13bf90940aa63f67f96eff
Alex Gordeev <gordeeffff@gmail.com>
2020-10-21 21:03:23 +0300

    solve morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,45 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    let phrase = [];
+    let char = '';
+    let letter = '';
+
+    for (let i = 0; i < expr.length; i ++) {
+        char += expr[i];
+        if(i % 10 === 9) {
+            if (char === '**********') {
+                letter = char;
+            } else {
+                for (let j = 0; j < 10; j += 2) {
+                    let morseLetter = '';
+                    if (char[j] === '0') {
+                        morseLetter += '';
+                    } else if (char[j] === '1' && char [j + 1] === '0') {
+                        morseLetter += '.';
+                    } else if (char[j] === '1' && char [j + 1] === '1') {
+                        morseLetter += '-';
+                    }
+                    letter += morseLetter;
+                }
+            }
+            phrase.push(MORSE_TABLE[letter]);
+            letter = '';
+            char = '';
+        }
+    }
+
+    res = phrase.join('');
+    return res;
+}
+
+checkChar = (key) => {
+
 }
 
 module.exports = {
